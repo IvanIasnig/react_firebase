0e182a3ec2691906eae8f502114fa8ce07f85379
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-06-07 19:35:09 +0200

    ciao

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,24 +1,22 @@
-import { useState } from "react"
-import { auth } from "../config/firebase"
-import { createUserWithEmailAndPassword } from "firebase/auth"
+import { useState } from "react";
+import { auth } from "../config/firebase";
+import { createUserWithEmailAndPassword } from "firebase/auth";
 
 const Auth = () => {
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
-    const [email, setEmail] = useState("")
-    const [password, setPassword] = useState("")
-
-
-    const signIn = async () =>{
-        await createUserWithEmailAndPassword(auth, email, password)
-    };
+  const signIn = async () => {
+    await createUserWithEmailAndPassword(auth, email, password);
+  };
 
   return (
     <div>
-      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)}></input>
-      <input placeholder="Password" onChange={(e) => setPassword(e.target.value)}></input>
-      <button>Sign In</button>
+      <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
+      <input placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <button onClick={signIn}>Sign In</button>
     </div>
-  )
-}
+  );
+};
 
-export default Auth
+export default Auth;
