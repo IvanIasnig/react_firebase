84baa2272f81f8212e6ca671e9abb40c6970bcdc
ivan1998iasnig@gmail.com <ivan1998iasnig@gmail.com>
2023-06-07 20:00:51 +0200

    ciao

--- src/components/auth.js
+++ src/components/auth.js
@@ -1,20 +1,48 @@
 import { useState } from "react";
-import { auth } from "../config/firebase";
-import { createUserWithEmailAndPassword } from "firebase/auth";
+import { auth, googleProvider } from "../config/firebase";
+import {createUserWithEmailAndPassword, signInWithPopup, signOut } from "firebase/auth";
 
 const Auth = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
   const signIn = async () => {
-    await createUserWithEmailAndPassword(auth, email, password);
+    try {
+      await createUserWithEmailAndPassword(auth, email, password);
+    } catch (err) {
+      console.error(err);
+    }
   };
+
+  const signInWithGoogle = async () => {
+    try {
+      await signInWithPopup(auth, googleProvider);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  const logout = async () => {
+    try {
+      await signOut(auth);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+
 
   return (
     <div>
       <input placeholder="Email" onChange={(e) => setEmail(e.target.value)} />
-      <input placeholder="Password" onChange={(e) => setPassword(e.target.value)} />
+      <input
+        placeholder="Password"
+        onChange={(e) => setPassword(e.target.value)}
+      />
       <button onClick={signIn}>Sign In</button>
+
+      <button onClick={signInWithGoogle}> Sign in with Google</button>
+      <button onClick={logout}>Sign Out</button>
     </div>
   );
 };
